5eea9b6fe208a1562425c9e622c50593763f6232
ivan <ivan.safonov.09@mail.ru>
2022-01-22 22:33:26 +0300

    ok\

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         <div className='content'>
           <Routes>
             <Route path='/dialogs' element={<Dialogs />} />
-            <Route path='/profile' element={<Profile />} />ll
+            <Route path='/profile' element={<Profile />} />
           </Routes>
         </div>
       </div>
