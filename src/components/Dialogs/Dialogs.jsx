1efe1e12ce8a170608c552b7fea10d9f5d5a7561
ivan <ivan.safonov.09@mail.ru>
2022-01-24 01:17:12 +0300

    Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,33 @@ import s from './Dialogs.module.css';
 
 const Dialogs =()=>{
     return(
-        <p>Dialogs</p>
+        <div className={s.dialogs}>
+            <div className={s.dialogs_item}>
+                <h2>Dialogs</h2>
+                <ul>
+                    <li className={s.dialog+" "+s.active}>Dima</li>
+                    <li className={s.dialog}>Oleg</li>
+                    <li className={s.dialog}>Valera</li>
+                    <li className={s.dialog}>Ola</li>
+                    <li className={s.dialog}>Polya</li>
+                    <li className={s.dialog}>Sven</li>
+                </ul>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    <div className={s.img}></div>
+                    <p>Hi</p>
+                </div>
+                <div className={s.message}>
+                <div className={s.img}></div>
+                    <p>How are you</p> 
+                </div>
+                <div className={s.message}>
+                    <div className={s.img}></div>
+                    <p>Welcom</p> 
+                </div>
+            </div>
+        </div>
     )
 }
 
