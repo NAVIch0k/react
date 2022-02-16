a9125d79101681370407f99338ba90aee281cdbd
ivan <ivan.safonov.09@mail.ru>
2022-02-16 22:31:00 +0300

    less64(full+extansion)

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -4,6 +4,7 @@ import Dialog_reducer from './Dialog_reducer';
 import Navbar_reducer from './Navbar_reducer';
 import Users_reducer from "./Users_reducer";
 import Auth_reducer from "./Auth_reducer";
+import {composeWithDevTools} from 'redux-devtools-extension'
 let reducers = combineReducers({
     profile_page: Profile_reducer,
     dialog_page: Dialog_reducer,
@@ -12,6 +13,6 @@ let reducers = combineReducers({
     auth:Auth_reducer
 })
 
-let store = createStore(reducers)
+let store = createStore(reducers,composeWithDevTools())
 window.store=store
 export default store
