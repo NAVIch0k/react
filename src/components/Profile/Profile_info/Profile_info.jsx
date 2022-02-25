8d422cba058df4cc4cd10b6a137cbb882c639aee
ivan <ivan.safonov.09@mail.ru>
2022-02-25 23:00:05 +0300

    less71(begin)

--- src/components/Profile/Profile_info/Profile_info.jsx
+++ src/components/Profile/Profile_info/Profile_info.jsx
@@ -14,6 +14,8 @@ const Profile_info = (props) => {
                 <img className={s.profile_info_img} src={props.profile.photos.small}/>
                 <div className={s.profile_info_about}>
                     <h2>{props.profile.fullName}</h2>
+                    <span>{props.status}</span>
+                    <input type="text" value={props.status} />
                     <p>{props.profile.aboutMe}</p>
                     <p>{props.profile.lookingForAJobDescription}</p>
                     <p>{props.profile.contacts.vk}</p>
